5970d2ade44f984692a58a8ec2762c56101674f0
pichus <dimatyzuk@gmail.com>
2025-11-19 13:33:15 +0200

    initial

---
+++ XMLParser/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace XMLParser.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ XMLParser/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace XMLParser.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
