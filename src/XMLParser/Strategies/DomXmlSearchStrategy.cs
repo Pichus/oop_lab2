3f3f1784802f122e57d27c96fc5d56677fa5cdee
pichus <dimatyzuk@gmail.com>
2025-11-22 15:37:01 +0200

    rewrite strategies

--- src/XMLParser/Strategies/DomXmlSearchStrategy.cs
+++ src/XMLParser/Strategies/DomXmlSearchStrategy.cs
@@ -1,100 +1,116 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
 using XMLParser.Models;
 
-namespace XMLParser.Strategies;
-
-public class DomXmlSearchStrategy : IXmlSearchStrategy
+namespace XMLParser.Strategies
 {
-    public string Name => "DOM (XmlDocument)";
-
-    public IEnumerable<SearchResult> Search(
-        string xmlPath,
-        string? attributeName,
-        string? attributeValue,
-        string? keyword)
+    public class DomXmlSearchStrategy : IXmlSearchStrategy
     {
-        var doc = new XmlDocument();
-        doc.Load(xmlPath);
+        public string Name => "DOM (XmlDocument)";
 
-        var nodes = doc.SelectNodes("//book");
-        if (nodes is null) yield break;
-
-        foreach (XmlNode node in nodes)
+        public IEnumerable<SearchResult> Search(
+            string xmlPath,
+            string nodeName,
+            string? attributeName,
+            string? attributeValue,
+            string? keyword)
         {
-            var attrSummary = new List<string>();
-            string? foundAttrValue = null;
+            var doc = new XmlDocument();
+            doc.Load(xmlPath);
 
-            if (node.Attributes is not null)
-                foreach (XmlAttribute attr in node.Attributes)
-                {
-                    attrSummary.Add($"{attr.Name}={attr.Value}");
-                    if (attributeName is not null && attr.Name == attributeName)
-                        foundAttrValue = attr.Value;
-                }
+            var nodes = doc.GetElementsByTagName(nodeName);
+            if (nodes == null) yield break;
 
-            var attrMatches = attributeName is null
-                              || attributeValue is null
-                              || foundAttrValue == attributeValue;
-
-            var innerText = node.InnerText;
-            var keywordMatches = string.IsNullOrWhiteSpace(keyword)
-                                 || innerText.Contains(keyword,
-                                     StringComparison.OrdinalIgnoreCase);
-
-            if (attrMatches && keywordMatches)
-                yield return new SearchResult
-                {
-                    NodeName = node.Name,
-                    AttributeSummary = string.Join(", ", attrSummary),
-                    TextContent = innerText
-                };
-        }
-    }
-
-    public IEnumerable<string> GetAttributeNames(string xmlPath)
-    {
-        var names = new HashSet<string>();
-        var doc = new XmlDocument();
-        doc.Load(xmlPath);
-
-        var nodes = doc.SelectNodes("//book");
-        if (nodes is null) return names;
-
-        foreach (XmlNode node in nodes)
-        {
-            if (node.Attributes is null) continue;
-            foreach (XmlAttribute attr in node.Attributes) names.Add(attr.Name);
-        }
-
-        return names;
-    }
-
-    public IDictionary<string, HashSet<string>> GetAttributeValues(string xmlPath)
-    {
-        var dict = new Dictionary<string, HashSet<string>>();
-        var doc = new XmlDocument();
-        doc.Load(xmlPath);
-
-        var nodes = doc.SelectNodes("//book");
-        if (nodes is null) return dict;
-
-        foreach (XmlNode node in nodes)
-        {
-            if (node.Attributes is null) continue;
-            foreach (XmlAttribute attr in node.Attributes)
+            foreach (XmlNode node in nodes)
             {
-                if (!dict.TryGetValue(attr.Name, out var set))
+                // Collect child ELEMENTS as field/value pairs
+                var childElements = node.ChildNodes
+                    .OfType<XmlElement>()
+                    .ToList();
+
+                var attrSummary = new List<string>();
+                string? foundAttrValue = null;
+
+                foreach (var el in childElements)
                 {
-                    set = new HashSet<string>();
-                    dict[attr.Name] = set;
+                    var key = el.Name;
+                    var val = el.InnerText ?? string.Empty;
+
+                    attrSummary.Add($"{key}={val}");
+
+                    if (attributeName != null && string.Equals(key, attributeName, StringComparison.OrdinalIgnoreCase))
+                        foundAttrValue = val;
                 }
 
-                set.Add(attr.Value);
+                bool attrMatches =
+                    attributeName is null ||
+                    attributeValue is null ||
+                    foundAttrValue == attributeValue;
+
+                var innerText = node.InnerText ?? string.Empty;
+
+                bool keywordMatches =
+                    string.IsNullOrWhiteSpace(keyword) ||
+                    innerText.Contains(keyword, StringComparison.OrdinalIgnoreCase);
+
+                if (attrMatches && keywordMatches)
+                {
+                    yield return new SearchResult
+                    {
+                        NodeName = node.Name,
+                        AttributeSummary = string.Join(", ", attrSummary),
+                        TextContent = innerText
+                    };
+                }
             }
         }
 
-        return dict;
+        public IEnumerable<string> GetAttributeNames(string xmlPath, string nodeName)
+        {
+            var names = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            var nodes = doc.GetElementsByTagName(nodeName);
+            if (nodes == null) return names;
+
+            foreach (XmlNode node in nodes)
+            {
+                foreach (XmlElement el in node.ChildNodes.OfType<XmlElement>())
+                    names.Add(el.Name);
+            }
+
+            return names;
+        }
+
+        public IDictionary<string, HashSet<string>> GetAttributeValues(string xmlPath, string nodeName)
+        {
+            var dict = new Dictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase);
+            var doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            var nodes = doc.GetElementsByTagName(nodeName);
+
+            foreach (XmlNode node in nodes)
+            {
+                foreach (XmlElement el in node.ChildNodes.OfType<XmlElement>())
+                {
+                    var key = el.Name;
+                    var val = el.InnerText ?? string.Empty;
+
+                    if (!dict.TryGetValue(key, out var set))
+                    {
+                        set = new HashSet<string>();
+                        dict[key] = set;
+                    }
+
+                    set.Add(val);
+                }
+            }
+
+            return dict;
+        }
     }
 }
--- src/XMLParser/Strategies/LinqXmlSearchStrategy.cs
+++ src/XMLParser/Strategies/LinqXmlSearchStrategy.cs
@@ -12,65 +12,61 @@ public class LinqXmlSearchStrategy : IXmlSearchStrategy
 
     public IEnumerable<SearchResult> Search(
         string xmlPath,
+        string nodeName,
         string? attributeName,
         string? attributeValue,
         string? keyword)
     {
         var doc = XDocument.Load(xmlPath);
 
-        var query = doc.Root?
-            .Elements("book") ?? [];
+        var nodes = doc.Descendants(nodeName);
 
-        foreach (var book in query)
+        foreach (var node in nodes)
         {
-            var attrs = book.Attributes().ToList();
+            var attrs = node.Elements().ToList();
 
-            string? foundAttrValue = null;
-            if (attributeName is not null)
-            {
-                var attr = attrs.FirstOrDefault(a => a.Name.LocalName == attributeName);
-                foundAttrValue = attr?.Value;
-            }
+            var foundAttrValue = attributeName is not null
+                ? attrs.FirstOrDefault(a => a.Name.LocalName == attributeName)?.Value
+                : null;
 
-            var attrMatches = attributeName is null
-                              || attributeValue is null
-                              || foundAttrValue == attributeValue;
+            var attrMatches =
+                attributeName is null ||
+                attributeValue is null ||
+                foundAttrValue == attributeValue;
 
-            var innerText = string.Join(" ", book.Elements().Select(e => e.Value));
-            var keywordMatches = string.IsNullOrWhiteSpace(keyword)
-                                 || innerText.Contains(keyword,
-                                     StringComparison.OrdinalIgnoreCase);
+            var innerText = string.Join(" ", node.Elements().Select(e => e.Value));
+
+            var keywordMatches =
+                string.IsNullOrWhiteSpace(keyword) ||
+                innerText.Contains(keyword, StringComparison.OrdinalIgnoreCase);
 
             if (attrMatches && keywordMatches)
                 yield return new SearchResult
                 {
-                    NodeName = book.Name.LocalName,
+                    NodeName = node.Name.LocalName,
                     AttributeSummary = string.Join(", ", attrs.Select(a => $"{a.Name}={a.Value}")),
                     TextContent = innerText
                 };
         }
     }
 
-    public IEnumerable<string> GetAttributeNames(string xmlPath)
+    public IEnumerable<string> GetAttributeNames(string xmlPath, string nodeName)
     {
         var doc = XDocument.Load(xmlPath);
 
-        return doc.Root?
-                   .Elements("book")
-                   .SelectMany(b => b.Attributes())
-                   .Select(a => a.Name.LocalName)
-                   .Distinct()
-               ?? [];
+        return doc.Descendants(nodeName)
+            .SelectMany(n => n.Elements())
+            .Select(a => a.Name.LocalName)
+            .Distinct();
     }
 
-    public IDictionary<string, HashSet<string>> GetAttributeValues(string xmlPath)
+    public IDictionary<string, HashSet<string>> GetAttributeValues(string xmlPath, string nodeName)
     {
         var doc = XDocument.Load(xmlPath);
+
         var dict = new Dictionary<string, HashSet<string>>();
 
-        var books = doc.Root?.Elements("book") ?? [];
-        foreach (var b in books)
-        foreach (var attr in b.Attributes())
+        foreach (var attr in doc.Descendants(nodeName).SelectMany(n => n.Elements()))
         {
             if (!dict.TryGetValue(attr.Name.LocalName, out var set))
             {
